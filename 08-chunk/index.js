284c7fd372a5145a357b69bd0c3d1dd60e807139
PavloPichuzhkin <pichujkin1986@gmail.com>
2021-09-13 18:10:01 +0300

    without 10

--- 08-chunk/index.js
+++ 08-chunk/index.js
@@ -1,5 +1,11 @@
 // TODO: rename function, maybe array-of-arrays
 
 export const chunk = (arr = [], size = 0) => {
+    let ar = [];
+    for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+        ar[i] = arr.slice((i*size), (i*size) + size);
 
+    }
+    
+    return ar;
 };
--- 09-sameParityFilter/index.js
+++ 09-sameParityFilter/index.js
@@ -1,3 +1,18 @@
 export const sameParityFilter = (arr = []) => {
+    let ar = [];
+    if (arr[0] % 2 === 0) {
+        ar = arr.filter(elem => {
+            // if (elem % 2 === 0) return elem;//?????????
+            return elem % 2 === 0;
+        })
+    }
+    else {
+        ar = arr.filter(elem => {
+        // if (elem%2===1) return elem; //???????????
+            // return elem % 2 === 1;
+            return !(elem % 2 === 0);
+        })
+    }
 
+    return ar;
 }
